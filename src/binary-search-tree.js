2df12248e7557798535bfb9a233a3a0fa92b6ad4
Nikita K <w.feelingalright.p@yandex.ru>
2022-05-02 17:33:39 +0300

    feat: add binary search tree solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,40 +7,137 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor(data = null) {
+      this.currRoot = data;
+      this.values = [];
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      return this.currRoot ? this.currRoot : null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  createNode(data) {
+      return {
+          data: data,
+          right: null,
+          left: null
+      }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  nextLevelNode(node, data) {
+      if (!node) {
+          return this.createNode(data);
+      }
+
+      if (data === node.data) {
+          return node;
+      }
+
+      if (data > node.data) {
+          node.right = this.nextLevelNode(node.right, data);
+      } else {
+          node.left = this.nextLevelNode(node.left, data);
+      }
+
+      return node;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  hasValueNode(node, data) {
+      if (!node) return null;
+
+      if (node.data === data) {
+          return node;
+      }
+
+      if (data > node.data) {
+          return this.hasValueNode(node.right, data);
+      } else {
+          return this.hasValueNode(node.left, data);
+      }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  leftTraversal(node = this.currRoot) {
+      if (node) {
+          if (node.left) {
+              this.leftTraversal(node.left);
+          }
+          if (node.right) {
+              this.leftTraversal(node.right);
+          }
+      } else {
+          return;
+      }
+
+      this.values.push(node.data);
+      return this.values;
   }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeNode(node, data) {
+      if (!node) return;
+
+      if (data > node.data) {
+          let check = this.removeNode(node.right, data);
+          if (check) node.right = null;
+      } else if (data < node.data) {
+          let check = this.removeNode(node.left, data);
+          if (check) node.left = null;
+      }
+
+      if (node.data === data) {
+          if (node.right) {
+              let currValue = this.min(node.right);
+              node.data = currValue;
+
+              let check = this.removeNode(node.right, currValue);
+              if (check) node.right = null;
+          } else if (node.left) {
+              let currValue = this.max(node.left);
+              node.data = currValue;
+
+              let check = this.removeNode(node.left, currValue);
+              if (check) node.left = null;
+          } else {
+              return node;
+          }
+      }
   }
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+      this.currRoot = this.nextLevelNode(this.currRoot, data);
+  }
+
+  has(data) {
+      let hasData = this.hasValueNode(this.currRoot, data);
+
+      return hasData ? true : false;
+  }
+
+  find(data) {
+      let findData = this.hasValueNode(this.currRoot, data);
+
+      return findData ? findData : null;
+  }
+
+  min(node = this.currRoot) {
+      let arr = this.leftTraversal(node);
+      this.values = [];
+
+      if (arr) {
+          return Math.min(...arr);
+      }
+  }
+
+  max(node = this.currRoot) {
+      let arr = this.leftTraversal(node);
+      this.values = [];
+
+      if (arr) {
+          return Math.max(...arr);
+      }
+  }
+
+  remove(data) {
+      return this.removeNode(this.currRoot, data);
   }
 }
 
